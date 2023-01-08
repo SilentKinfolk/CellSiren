98ed867d06eba2aa81dbf781608d1f0fb266fe7b
SilentKinfolk <{ID}+{username}@users.noreply.github.com>
2023-01-08 21:16:31 +0000

    Add updated app with icons

--- src/hello_world.py
+++ src/hello_world.py
@@ -1,19 +1,35 @@
-from PyQt6 import QtWidgets
+from PyQt6.QtWidgets import QMainWindow, QApplication, QLabel, QVBoxLayout, QPushButton, QWidget
+from PyQt6.QtGui import QIcon
+import sys, os
 
-import sys
+basedir = os.path.dirname(__file__)
 
-class MainWindow(QtWidgets.QMainWindow):
+
+class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("Hello World")
-        l = QtWidgets.QLabel("My simple app.")
-        l.setMargin(10)
-        self.setCentralWidget(l)
+        layout = QVBoxLayout()
+        label = QLabel("My simple app.")
+        label.setMargin(10)
+        layout.addWidget(label)
+
+        button = QPushButton("Push")
+        button.setIcon(QIcon(os.path.join(basedir, "icons", "lightning.svg")))
+        button.pressed.connect(self.close)
+        layout.addWidget(button)
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
+
         self.show()
 
 if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon(os.path.join(basedir, "icons", "penguin.svg")))
     w = MainWindow()
     app.exec()
--- src/hello_world.spec
+++ src/hello_world.spec
@@ -7,28 +7,33 @@ block_cipher = None
 a = Analysis(['hello_world.py'],
              pathex=[],
              binaries=[],
-             datas=[],
+             datas=[('icons', 'icons')],
              hiddenimports=[],
              hookspath=[],
+             hooksconfig={},
              runtime_hooks=[],
              excludes=[],
              win_no_prefer_redirects=False,
              win_private_assemblies=False,
              cipher=block_cipher,
              noarchive=False)
-pyz = PYZ(a.pure, a.zipped_data, cipher=block_cipher)
+pyz = PYZ(a.pure, a.zipped_data,
+             cipher=block_cipher)
 
 exe = EXE(pyz,
           a.scripts,
           [],
           exclude_binaries=True,
-          name='hello_world',
+          name='hello-world',
           debug=False,
           bootloader_ignore_signals=False,
           strip=False,
           upx=True,
-          console=False
-         )
+          console=False,
+          disable_windowed_traceback=False,
+          target_arch=None,
+          codesign_identity=None,
+          entitlements_file=None )
 coll = COLLECT(exe,
                a.binaries,
                a.zipfiles,
